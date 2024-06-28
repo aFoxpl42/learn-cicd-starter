00bd965b6f5191fccd428172322323804e470c75
aFoxpl42 <jonskid44@gmail.com>
2024-06-28 13:33:44 +0200

    Added tests. Broken code CI test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,60 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name          string
+		headers       http.Header
+		expectedKey   string
+		expectedError error
+	}{
+		{
+			name:          "No Authorization header",
+			headers:       http.Header{},
+			expectedKey:   "",
+			expectedError: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization header - missing ApiKey",
+			headers: http.Header{
+				"Authorization": []string{"Bearer somekey"},
+			},
+			expectedKey:   "",
+			expectedError: ErrMalformedAuthorizationHeader,
+		},
+		{
+			name: "Malformed Authorization header - missing key",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey"},
+			},
+			expectedKey:   "",
+			expectedError: nil,
+		},
+		{
+			name: "Correct Authorization header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey somekey"},
+			},
+			expectedKey:   "somekey",
+			expectedError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			key, err := GetAPIKey(tt.headers)
+			if key != tt.expectedKey {
+				t.Errorf("expected key %q, got %q", tt.expectedKey, key)
+			}
+			if !errors.Is(err, tt.expectedError) {
+				t.Errorf("expected error %v, got %v", tt.expectedError, err)
+			}
+		})
+	}
+}
